a5d7c512d67c856132d1499262524eab304b8474
Iwasawafag <iwasawafag@yandex.ru>
2016-07-08 08:11:43 +0200

    Add Posterizer class built on top of Potrace

--- test/test.js
+++ test/test.js
@@ -1,18 +1,25 @@
 'use strict';
 
 var Potrace = require('../lib/index').Potrace,
+    Posterizer = require('../lib/index').Posterizer,
     potrace = new Potrace(),
+    posterize,
     fs = require('fs');
 
 potrace.loadImage('./yao.jpg', function(err) {
-  if (err) {
-    throw err;
-  }
+  if (err) { throw err; }
 
   fs.writeFileSync('./output-x1.svg', potrace.getSVG(1));
   fs.writeFileSync('./output-x2.svg', potrace.getSVG(2));
   fs.writeFileSync('./output-x0.5.svg', potrace.getSVG(0.5));
   fs.writeFileSync('./output-x0.25.svg', potrace.getSVG(0.25));
 
-  fs.writeFileSync('./output-x1-curves.svg', potrace.getSVG(1, 'curve'));
+  fs.writeFileSync('./output-x1-curves.svg', potrace.getSVG(1, true));
+});
+
+posterize = new Posterizer({ blackLevel: 200, samples: 4 });
+
+posterize.loadImage('./yao.jpg', function(err) {
+  if (err) { throw err; }
+  fs.writeFileSync('./output-posterized-x1.svg', posterize.getSVG(1));
 });
