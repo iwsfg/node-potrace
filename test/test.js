99cbd7eaf1bf7a42874e2c4f4b6228835b635569
Iwasawafag <iwasawafag@yandex.ru>
2016-07-18 14:07:01 +0200

    Remove scale parameter from all public methods

--- test/test.js
+++ test/test.js
@@ -2,22 +2,17 @@
 
 var Potrace = require('../lib/index').Potrace,
     Posterizer = require('../lib/index').Posterizer,
-    potrace = new Potrace(),
-    posterize,
-    fs = require('fs');
+    fs = require('fs'),
+    potrace, posterize;
 
+potrace = new Potrace();
 potrace.loadImage('./yao.jpg', function(err) {
   if (err) { throw err; }
-
-  fs.writeFileSync('./output-x1.svg', potrace.getSVG(1));
-  fs.writeFileSync('./output-x2.svg', potrace.getSVG(2));
-  fs.writeFileSync('./output-x0.5.svg', potrace.getSVG(0.5));
-  fs.writeFileSync('./output-x0.25.svg', potrace.getSVG(0.25));
+  fs.writeFileSync('./output.svg', potrace.getSVG());
 });
 
 posterize = new Posterizer({ blackLevel: 200, samples: 4 });
-
 posterize.loadImage('./yao.jpg', function(err) {
   if (err) { throw err; }
-  fs.writeFileSync('./output-posterized-x1.svg', posterize.getSVG(1));
+  fs.writeFileSync('./output-posterized.svg', posterize.getSVG());
 });
