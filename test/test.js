9e2644057566a44bcf3d8bcc98e5effdefe7297b
Iwasawafag <iwasawafag@yandex.ru>
2016-08-23 03:37:10 +0200

    Accept Jimp instances as image source

--- test/test.js
+++ test/test.js
@@ -1,7 +1,8 @@
 'use strict';
 
 var lib = require('../lib/index'),
-    fs = require('fs');
+    fs = require('fs'),
+    Jimp = require('jimp');
 
 lib.trace('./yao.jpg', function(err, svg) {
   if (err) { throw err; }
@@ -11,6 +12,17 @@ lib.trace('./yao.jpg', function(err, svg) {
 lib.posterize('./yao.jpg', function(err, svg) {
   if (err) { throw err; }
   fs.writeFileSync('./output-posterized.svg', svg);
+});
+
+// Jimp instance as image source
+
+Jimp.read('./yao.jpg', function(err, img) {
+  if (err) { throw err; }
+
+  lib.trace(img, function(err, svg) {
+    if (err) { throw err; }
+    fs.writeFileSync('./output-from-jimp-instance.svg', svg);
+  });
 });
 
 // Generating example for readme with hand-picked thresholds
